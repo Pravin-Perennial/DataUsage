35df3cae2a6cfd1de364cf94cd4213eb4f1836e7
pravin <pravin.jadhao@perennialsys.com>
2020-08-17 10:48:41 +0530

    Created the project

---
+++ DataUsage/DataUsage/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  DataUsage
+//
+//  Created by Pere-Dev on 17/08/20.
+//  Copyright © 2020 Perennial System. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ DataUsage/DataUsage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataUsage
+//
+//  Created by Pere-Dev on 17/08/20.
+//  Copyright © 2020 Perennial System. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
